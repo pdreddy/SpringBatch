8805a3a2935f888025d5fb755bdeb52bf4e1849d
pdreddy <8177670+pdreddy@users.noreply.github.com>
2019-07-12 11:56:15 -0700

    Update Constants.java

--- sp_batch_xml_db/src/main/java/com/damu/demo/springboot/util/Constants.java
+++ sp_batch_xml_db/src/main/java/com/damu/demo/springboot/util/Constants.java
@@ -1,6 +1,6 @@
 package com.damu.demo.springboot.util;
 
 public class Constants {
-		public static final String	INSERT_QUERY_OLD = "INSERT INTO TEST_VEHICLE(ID,vehiclenumber,brand,country,modelname,modelyear)  VALUES(provisionvehicle_seq.nextval,?,?,?,?,?)";
+		public static final String	INSERT_QUERY_OLD = "INSERT INTO TEST_VEHICLE(ID,vehiclenumber,brand,country,modelname,modelyear)  VALUES(vehicle_seq.nextval,?,?,?,?,?)";
 	
 }
